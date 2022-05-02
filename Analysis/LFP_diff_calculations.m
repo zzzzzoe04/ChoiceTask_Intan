40a0fe5206f033d8aa3a08deb72c12da0a2f10f7
Jennifer Magnusson <54630297+jmagnu1@users.noreply.github.com>
2022-05-02 10:22:54 -0400

    working on functions for lfp_diff

--- Analysis/LFP_diff_calculations.m
+++ Analysis/LFP_diff_calculations.m
@@ -1,19 +1,29 @@
 % re-arrange monopolar field potentials into the order on shanks
-num_sites = size(lfp, 1);   % number of sites = number of rows in original lfp array
-num_lfp_points = size(lfp, 2);   %n number of points = number of columns in original lfp array
-% pre-allocate array
-lfps_by_shank = zeros(num_sites, num_lfp_points);
-num_shanks = 8;
-sites_per_shank = 8;
-for i_shank = 1 : num_shanks            % shank number
-    for i_site = 1 : num_shanks         % site number on each shank
-        lfp_row = (i_shank - 1) * 8 + i_site; % If i_shank = 1 then i_shank - 1 = 0 then this whole equation is 0 regardless of i_site? JM 20220501
-        % look up the row in the table containing the site ID - don't have that with me so 
-        % will need to look up variable names, etc.
-        orig_lfp_row = % code here to figure out which row is i_site on i_shank in original lfp array
-        lfps_by_shank(lfp_row, :) = lfp(orig_lfp_row, :);
-    end
-end
+
+% Test code to write in NNsite order; function lfp_NNsite =
+% probe_site_mapping('NN8x8'); essentially does what this block is trying
+% to do
+
+% num_sites = size(lfp, 1);   % number of sites = number of rows in original lfp array
+% num_lfp_points = size(lfp, 2);   %n number of points = number of columns in original lfp array
+% % pre-allocate array
+% lfps_by_shank = zeros(num_sites, num_lfp_points);
+% num_shanks = 8;
+% sites_per_shank = 8;
+% for i_shank = 1 : num_shanks            % shank number
+%     for i_site = 1 : num_shanks         % site number on each shank
+%         lfp_row = (i_shank - 1) * 8 + i_site; % If i_shank = 1 then i_shank - 1 = 0 then this whole equation is 0 regardless of i_site? JM 20220501
+%         % look up the row in the table containing the site ID - don't have that with me so 
+%         % will need to look up variable names, etc.
+%         orig_lfp_row = % code here to figure out which row is i_site on i_shank in original lfp array
+%         lfps_by_shank(lfp_row, :) = lfp(orig_lfp_row, :);
+%     end
+% end
+
+%% 
+
+lfp_NNsite = probe_site_mapping('NN8x8');
+
 % pre-allocate memory for differential LFPs
 num_diff_rows = num_sites - num_shanks;
 diff_lfps = zeros(num_diff_rows, num_lfp_points); 
