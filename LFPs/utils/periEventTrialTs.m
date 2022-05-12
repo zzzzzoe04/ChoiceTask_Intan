9425eef119c758d231d380da62708d64b29cf324
Jennifer Magnusson <54630297+jmagnu1@users.noreply.github.com>
2022-05-12 10:03:56 -0400

    making edits

--- LFPs/utils/periEventTrialTs.m
+++ LFPs/utils/periEventTrialTs.m
@@ -1,4 +1,18 @@
 function trialRanges = periEventTrialTs(trials,tWindow,eventFieldnames)
+%
+%
+% INPUTS
+%   trials - trials structure
+%   tWindow - two-element array containing length of time (in seconds)
+%       before and after each event to extract. Note that both numbers
+%       should be positive
+%   eventFieldnames - length of event fields for which to extract time
+%       windows
+%
+% OUTPUTS
+%   trialRanges - m x n x 2 array, where m is the number of fields being
+%       analyzed, n is the number of trials, and the last two elements
+%       contain the start and end time for each window
 
 trialRanges = NaN(numel(eventFieldnames),numel(trials),2);
 for iField = 1:numel(eventFieldnames)
