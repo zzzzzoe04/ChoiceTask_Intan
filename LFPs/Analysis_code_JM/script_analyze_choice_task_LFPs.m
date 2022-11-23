65226059cd02199bde6f2453b0142be3990ea826
Jennifer Magnusson <54630297+jmagnu1@users.noreply.github.com>
2022-11-23 17:23:23 -0500

    updating lfp workflow

--- LFPs/Analysis_code_JM/script_analyze_choice_task_LFPs.m
+++ LFPs/Analysis_code_JM/script_analyze_choice_task_LFPs.m
@@ -7,8 +7,10 @@ rats_with_intan_sessions = find_rawdata_folders(intan_parent_directory);
 % test_folder = '/Volumes/SharedX/Neuro-Leventhal/data/ChoiceTask/R0327/R0327-rawdata/R0327_20191218a/R0327_20191218_ChVE_191218_140437';
 % cd(test_folder);
 
-sessions_to_ignore = {'R0427_20220920_Testing_220920_150255'}; % R0425_20220728a debugging because the intan side was left on for 15 hours; 
-% R0427_20220920a does not have an 'info.rhd' file
+
+sessions_to_ignore = {'R0378_20210507a', 'R0326_20191107a', 'R0425_20220728a', 'R0425_20220816b','R0427_20220920a'}; % R0425_20220728a debugging because the intan side was left on for 15 hours;
+sessions_to_ignore1 = {'R0425_20220728_ChVE_220728_112601', 'R0427_20220920_Testing_220920_150255', 'R0427_20220920a'}; 
+sessions_to_ignore2 = {'R0427_20220908a', 'R0427_20220909a', 'R0427_20220912a','R0427_20220913a', 'R0427_20220914a', 'R0427_20220915a', 'R0427_20220916a'}; % R0427_20220920a does not have an 'info.rhd' file
 
 %%
 for i_rat = 1 : length(rats_with_intan_sessions)
@@ -18,6 +20,9 @@ for i_rat = 1 : length(rats_with_intan_sessions)
     for i_sessionfolder = 1 : length(intan_folders)
         rd_metadata = parse_rawdata_folder(intan_folders{i_sessionfolder});
         pd_folder = create_processed_data_folder(rd_metadata, intan_parent_directory);
+        ratID = rd_metadata.ratID;
+        intan_session_name = rd_metadata.session_name;
+        session_name = rd_metadata.session_name;
         
         lfp_fname = fullfile(pd_folder, create_lfp_fname(rd_metadata));
         
@@ -31,9 +36,13 @@ for i_rat = 1 : length(rats_with_intan_sessions)
         session_name = pd_processed_data.session_name;
         
         if any(strcmp(session_name, sessions_to_ignore))
-            continue
+            continue;
         end
         
+        if contains(session_name, sessions_to_ignore) || contains(intan_session_name, sessions_to_ignore1)|| contains(ratID, 'DigiInputTest') % Just always ignore these sessions. R0411 no data, DigitInputTest is t est files
+            continue;
+        end
+
         [lfp, actual_Fs] = calculate_NNprobe_monopolar_LFPs(intan_folders{i_sessionfolder}, 500);
         
         save(lfp_fname, 'lfp', 'actual_Fs');
