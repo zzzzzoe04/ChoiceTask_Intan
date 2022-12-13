a127a4d8289dc3eb4587577a51ea21245c3149e8
Dan Leventhal <dleventh@med.umich.edu>
2022-12-13 18:33:02 -0500

    calculating trial structures

---
+++ ChoiceTask_Intan_behavior_analysis/create_and_save_trials_struct_Intan.m
@@ -0,0 +1,42 @@
+% script create_and_save_trials_struct_Intan
+
+% loop through all rats with valid choice task data, create trials
+% structure for each session, save in the processed data folder to save
+% time
+
+parent_directory = 'Z:\data\ChoiceTask\';
+
+[rat_nums, ratIDs, ratIDs_goodhisto] = get_rat_list();
+
+num_rats = length(ratIDs);
+
+for i_rat = 1 : num_rats
+    ratID = ratIDs{i_rat};
+    rat_folder = fullfile(parent_directory, ratID);
+
+    if ~isfolder(rat_folder)
+        continue;
+    end
+
+    processed_folder = find_data_folder(ratID, 'processed', parent_directory);
+    session_dirs = dir(fullfile(processed_folder, strcat(ratID, '*')));
+    num_sessions = length(session_dirs);
+
+    rawdata_folder = find_data_folder(ratID, 'rawdata', parent_directory);
+
+    for i_session = 1 : num_sessions
+        
+        session_name = session_dirs(i_session).name;
+        cur_processed_dir = fullfile(session_dirs(i_session).folder, session_name);
+        cur_rawdata_dir = fullfile(rawdata_folder, ratID, session_name);
+        cd(cur_processed_dir)
+
+        lfp_fname = strcat(session_name, '_lfp.mat');
+        if ~isfile(lfp_fname)
+            sprintf('%s not found, skipping', lfp_file)
+            continue
+        end
+
+    end
+
+end
--- LFPs/calculate_trial_spectrograms.m
+++ LFPs/calculate_trial_spectrograms.m
@@ -2,6 +2,14 @@
 % the processed data folders
 
 parent_directory = 'Z:\data\ChoiceTask\';
+summary_xls = 'ProbeSite_Mapping_MATLAB.xlsx';
+summary_xls_dir = 'Z:\data\ChoiceTask\Probe Histology Summary';
+summary_xls = fullfile(summary_xls_dir, summary_xls);
+
+probe_type_sheet = 'probe_type';
+probe_types = read_Jen_xls_summary(summary_xls, probe_type_sheet);
+% NOTE - UPDATE FUNCTION read_Jen_xls_summary WHEN WE NEED OTHER
+% INFORMATION OUT OF THAT SPREADSHEET
 
 [rat_nums, ratIDs, ratIDs_goodhisto] = get_rat_list();
 
@@ -15,12 +23,30 @@ for i_rat = 1 : num_rats
         continue;
     end
 
+    probe_type = probe_types{probe_types.RatID == ratID, 2};
     processed_folder = find_data_folder(ratID, 'processed', parent_directory);
-
     session_dirs = dir(fullfile(processed_folder, strcat(ratID, '*')));
-
     num_sessions = length(session_dirs);
 
     % WORKING HERE-NOW LOOP THROUGH, ORGANIZE DATA, CALCULATE SCALOGRAMS
+
+    for i_session = 1 : num_sessions
+        
+        session_name = session_dirs(i_session).name;
+        cur_dir = fullfile(session_dirs(i_session).folder, session_name);
+        cd(cur_dir)
+
+        lfp_fname = strcat(session_name, '_lfp.mat');
+        if ~isfile(lfp_fname)
+            sprintf('%s not found, skipping', lfp_file)
+            continue
+        end
+
+        lfp_data = load(lfp_fname);
+
+        Fs = lfp_data.actual_Fs;
+        [ordered_lfp, intan_site_order, intan_site_order_for_trials_struct, site_order] = lfp_by_probe_site_ALL(lfp_data, probe_type);
+
+    end
 
 end
