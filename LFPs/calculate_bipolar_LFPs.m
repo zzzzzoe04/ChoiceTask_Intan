1bcfb738728ee0d089cb1013b53a37137fe525bb
Dan Leventhal <dleventh@med.umich.edu>
2023-01-03 15:07:46 -0500

    Create calculate_bipolar_LFPs.m

---
+++ LFPs/calculate_bipolar_LFPs.m
@@ -0,0 +1,8 @@
+function bipolar_LFPs = calculate_bipolar_LFPs(lfp_data., probe_type)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
